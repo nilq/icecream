8208c4003e040d74c4232c14a0b59eb5798590e0
nilq <i.am@nilq.dk>
2017-03-10 09:17:37 +0100

    Implement Tagua-VM's LLVM type bindings

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use parser::lexer::Lexer;
 use vm::module::Module;
 use vm::context::Context;
 
+use vm::llvm_type::VMRepresentation;
+
 fn lexer_debug() {
     let input = "
 func foo(x: int, y: int) -> int
@@ -28,9 +30,14 @@ foo = 133.7 + \"I'm a string, hello\"
 
 fn ir_debug() {
     let context = Context::new();
-    let module  = Module::new("strawberry", &context);
+    {
+        let module  = Module::new("strawberry", &context);
 
-    println!("{}", module);
+        println!("{}", module);
+    }
+    
+    let val = "that rainbow kinda milk-based icecream";
+    println!("{}", val.to_representation(&context));
 
 }
 
--- src/vm/llvm_type.rs
+++ src/vm/llvm_type.rs
@@ -33,6 +33,21 @@ macro_rules! bind_llvm_type {
     )
 }
 
+bind_llvm_type!(LLVMInt1TypeInContext => int1_type);
+bind_llvm_type!(LLVMInt8TypeInContext => int8_type);
+bind_llvm_type!(LLVMInt16TypeInContext => int16_type);
+bind_llvm_type!(LLVMInt32TypeInContext => int32_type);
+bind_llvm_type!(LLVMInt64TypeInContext => int64_type);
+
+bind_llvm_type!(LLVMDoubleTypeInContext => double_type);
+bind_llvm_type!(LLVMFloatTypeInContext => float_type);
+
+bind_llvm_type!(LLVMFP128TypeInContext => fp128_type);
+
+bind_llvm_type!(LLVMVoidTypeInContext => void_type);
+bind_llvm_type!(LLVMX86FP80TypeInContext => x86fp80_type);
+bind_llvm_type!(LLVMX86MMXTypeInContext => x86mmx_type);
+
 pub fn int_type(size: u32, context: &Context) -> LLVMTypeRef {
     use vm::llvm::core::LLVMIntTypeInContext;
 
@@ -69,3 +84,84 @@ pub trait VMRepresentation {
     fn to_representation(self, context: &Context) -> Value;
 }
 
+macro_rules! to_int {
+    ($type_name:ty as $($alias:ty)as+ => $LLVM_type:ident($($LLVM_type_argument:expr),*)) => (
+        impl VMRepresentation for $type_name {
+            fn to_representation(self, context: &Context) -> Value {
+                use vm::llvm::core::$LLVM_type;
+
+                Value::from_ref(unsafe {
+                    LLVMConstInt(
+                        $LLVM_type(context.to_ref(), $($LLVM_type_argument), *),
+                        self as $($alias)as+,
+                        0 as LLVMBool,
+                    )
+                })
+            }
+        }
+    );
+
+    ($type_name:ty as $($alias:ty)as+ => $LLVM_type:ident) => (
+        to_int!{
+            $type_name as $($alias)as+ => $LLVM_type()
+        }
+    )
+}
+
+macro_rules! to_float {
+    ($type_name:ty as $alias:ty => $LLVM_type:ident) => (
+        impl VMRepresentation for $type_name {
+            fn to_representation(self, context: &Context) -> Value {
+                use vm::llvm::core::$LLVM_type;
+
+                Value::from_ref(unsafe {
+                    LLVMConstReal(
+                        $LLVM_type(context.to_ref()),
+                        self as $alias,
+                    )
+                })
+            }
+        }
+    )
+}
+
+to_int!(bool as c_ulonglong => LLVMInt1TypeInContext);
+
+to_int!(u8 as c_ulonglong => LLVMInt8TypeInContext);
+to_int!(i8 as c_ulonglong => LLVMInt8TypeInContext);
+
+to_int!(u16 as c_ulonglong => LLVMInt16TypeInContext);
+to_int!(i16 as c_ulonglong => LLVMInt16TypeInContext);
+
+to_int!(u32 as c_ulonglong => LLVMInt32TypeInContext);
+to_int!(i32 as c_ulonglong => LLVMInt32TypeInContext);
+
+to_int!(u64 as c_ulonglong => LLVMInt64TypeInContext);
+to_int!(i64 as c_ulonglong => LLVMInt64TypeInContext);
+
+to_int!(usize as c_ulonglong => LLVMIntTypeInContext(mem::size_of::<isize>() as c_uint * 8));
+to_int!(isize as c_ulonglong => LLVMIntTypeInContext(mem::size_of::<isize>() as c_uint * 8));
+
+to_int!(char as u32 as c_ulonglong => LLVMInt32TypeInContext)   ;
+
+to_float!(f32 as f64 => LLVMFloatTypeInContext);
+to_float!(f64 as f64 => LLVMDoubleTypeInContext);
+
+impl<'a> VMRepresentation for &'a str {
+    fn to_representation(self, context: &Context) -> Value {
+        self.as_bytes().to_representation(context)
+    }
+}
+
+impl<'a> VMRepresentation for &'a [u8] {
+    fn to_representation(self, context: &Context) -> Value {
+        Value::from_ref(unsafe {
+            LLVMConstStringInContext(
+                context.to_ref(),
+                self.as_ptr() as *const c_char,
+                self.len() as c_uint,
+                1 as LLVMBool,
+            )
+        })
+    }
+}
